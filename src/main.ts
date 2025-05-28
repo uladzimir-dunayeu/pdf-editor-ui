8ea0d5763a773d19a373bd7c94b506a5544626d6
Uladzimir Dunayeu <dunayeu.u@brainrocket.com>
2025-05-28 15:38:16 +0300

    add pdf generation logic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { PDFDocument } from 'pdf-lib';
+import fontkit from '@pdf-lib/fontkit';
+
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -13,12 +15,61 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="pdf-generate" type="button">Generate PDF</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.querySelector<HTMLDivElement>('#pdf-generate')!.addEventListener('click', async () => {
+    const existingPdfBytes = await fetch('../public/fw2g.pdf').then(res => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
+    pdfDoc.registerFontkit(fontkit);
+
+    const fontBytes = await fetch('../fonts/Courier-Prime-Sans.ttf').then((res) =>
+        res.arrayBuffer()
+    );
+    const customFont = await pdfDoc.embedFont(fontBytes);
+
+    const form = pdfDoc.getForm();
+    const fields = form.getFields();
+
+    fields.forEach((field) => {
+        const type = field.constructor.name;
+
+        if (type !== 'PDFCheckBox2' && !field.getName().includes('f1_01')) {
+            if (
+                field.getName().includes('f1_03') ||
+                field.getName().includes('f1_04') ||
+                field.getName().includes('f1_16') ||
+                field.getName().includes('f1_21')
+            ) {
+                const lastDotIndex = field.getName().lastIndexOf('.');
+                const result = field.getName().slice(lastDotIndex + 1);
+
+                form.getTextField(field.getName()).setText(result);
+            } else {
+                form
+                    .getTextField(field.getName())
+                    .setText(
+                        'Do Not Cut or Separate Forms on This Page Do Not Cut or Separate Forms on This Page Do Not Cut or Separate Forms on This Page Do Not Cut or Separate Forms on This Page Do Not Cut or Separate Forms on This Page Do Not Cut or Separate Forms on This Page Do Not Cut or Separate Forms on This Page '
+                    );
+                field.defaultUpdateAppearances(customFont);
+            }
+        }
+    });
+
+    const pdfBytes = await pdfDoc.save();
+
+    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'filled_form.pdf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+});
